d4692329706b65faa238526e89d33801c9f13b45
Dean Karres <karres@illinois.edu>
2023-06-01 09:50:29 -0500

    something is still odd

---
+++ SpeechAccessibility.Infrastructure/Data/ContributorFollowUpRepository.cs
@@ -0,0 +1,12 @@
+﻿using SpeechAccessibility.Core.Interfaces;
+using SpeechAccessibility.Core.Models;
+
+namespace SpeechAccessibility.Infrastructure.Data
+{
+    public class ContributorFollowUpRepository : Repository<SpeechAccessibilityDbContext, ContributorFollowUp>, IContributorFollowUpRepository
+    {
+        public ContributorFollowUpRepository(SpeechAccessibilityDbContext speechAccessibilityDbContext) : base(speechAccessibilityDbContext)
+        {
+        }
+    }
+}
